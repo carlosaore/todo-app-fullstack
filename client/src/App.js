227a26cd3ae2afb6aa570d91f88d0b570521daca
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-04-09 11:04:43 +0200

    connected backend and frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 function App() {
-  return (
-    <div>
-      <h1>To Do</h1>
-    </div>
-  );
+
+    function callApi() {
+        fetch("http://localhost:5000/api")
+            .then(response => response.json()).then(data => console.log(data));
+    }
+
+    return (
+        <div>
+            <h1>To Do</h1>
+            <button onClick={callApi}>click me</button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/api', (req, res) => {
+    const retro = [
+        {id: 1, text: 'This went well, etc.', upVotes: 3, downVotes: 0},
+        {id: 2, text: 'Yes and no.', upVotes: 4, downVotes: 3},
+        {id: 3, text: 'Nothing went well, etc.', upVotes: 1, downVotes: 0},
+    ];
+    res.status(201).json(retro);
+});
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    });
+}
+
+module.exports = app;
