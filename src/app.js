4fd70bb01dcb552e46d2e6209ac837169220dc99
Carlos Orellana <59698756+carlosaore@users.noreply.github.com>
2021-05-04 22:41:55 +0200

    Tried to fix disconnect issue, failed.

--- src/app.js
+++ src/app.js
@@ -11,13 +11,23 @@ app.use(express.json());
 // Allow cors policies
 app.use(cors())
 
+// Connecting to the DB
 connection.connect((err)=>{
     if(err) {
-        console.error(`Error trying to reach the DB. Error: ${err.stack}`)
+        console.error(`Error trying to reach the DB. Error: ${err}`)
         return;
     }
     console.log('Successfully connected to the DB')
-})
+});
+
+connection.on('error', err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        // db error reconnect
+        console.error('Disconnected by clearDB due to inactivity. You need to fix this');
+    } else {
+        throw err;
+    }
+});
 
 app.get('/api', cors(), (req, res) => {
 
